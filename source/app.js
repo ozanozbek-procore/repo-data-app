aa76f3efb9a791fc885822eb008cea57e2306878
ozanozbek-procore <ozan.ozbek@procore.com>
2025-02-03 14:42:29 +0100

    WIP

--- source/app.js
+++ source/app.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { Octokit } from 'octokit';
 
-const app = async (configuration) => {
+const app = async (configuration, verbose = false) => {
   const octokit = new Octokit({
     auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN
   });
@@ -9,19 +9,19 @@ const app = async (configuration) => {
   // const data = {};
   // configuration.repositories.forEach(repository => );
 
-  console.log(`Fetching ${configuration.repositories.length} repositories.`);
+  verbose && console.log(`Fetching ${configuration.repositories.length} repositories.`);
   const data = await configuration.repositories.reduce(async (acc, repository) => {  
-    console.log(` Fetching '${repository}'...`);
+    verbose && console.log(` Fetching '${repository}'...`);
     acc[repository] = (await octokit.paginate('GET /repos/{owner}/{repo}/pulls', {
       owner: configuration.owner,
       repo: repository,
       per_page: 100,
       state: 'closed'
     })).filter(pr => pr.title.includes(configuration.prTitleString));
-    console.log(` Done.`);
+    verbose && console.log(` Done.`);
     return acc;
   }, {});
-  console.log(`Done.`);
+  verbose && console.log(`Done.`);
 
   return data;
 };
--- source/main.js
+++ source/main.js
@@ -3,7 +3,7 @@ import fs from 'node:fs/promises';
 import app from './app.js';
 import configuration from '../configuration.json' with { type: 'json' };
 
-const data = await app(configuration);
+const data = await app(configuration, true);
 
 const date = new Date();
 const filename = `data/data-${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}.json`;
