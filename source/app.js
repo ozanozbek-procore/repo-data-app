6b4d3b46dc99f074e66db8ab5d5000e7e488d272
ozanozbek-procore <ozan.ozbek@procore.com>
2025-02-03 14:38:56 +0100

    WIP

--- source/app.js
+++ source/app.js
@@ -1,8 +1,29 @@
 import 'dotenv/config';
+import { Octokit } from 'octokit';
 
-const app = (configuration) => {
-  // console.log(configuration);
-  // console.log(process.env.GITHUB_PERSONAL_ACCESS_TOKEN);
+const app = async (configuration) => {
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_PERSONAL_ACCESS_TOKEN
+  });
+
+  // const data = {};
+  // configuration.repositories.forEach(repository => );
+
+  console.log(`Fetching ${configuration.repositories.length} repositories.`);
+  const data = await configuration.repositories.reduce(async (acc, repository) => {  
+    console.log(` Fetching '${repository}'...`);
+    acc[repository] = (await octokit.paginate('GET /repos/{owner}/{repo}/pulls', {
+      owner: configuration.owner,
+      repo: repository,
+      per_page: 100,
+      state: 'closed'
+    })).filter(pr => pr.title.includes(configuration.prTitleString));
+    console.log(` Done.`);
+    return acc;
+  }, {});
+  console.log(`Done.`);
+
+  return data;
 };
 
 export default app;
--- source/main.js
+++ source/main.js
@@ -1,4 +1,13 @@
+import fs from 'node:fs/promises';
+
 import app from './app.js';
 import configuration from '../configuration.json' with { type: 'json' };
 
-app(configuration);
+const data = await app(configuration);
+
+const date = new Date();
+const filename = `data/data-${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}.json`;
+
+console.log(`Writing to file ${filename}...`);
+await fs.writeFile(`${filename}`, JSON.stringify(data, undefined, 2));
+console.log('Done.');
