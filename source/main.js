f1c633586386500a4546744767646923170a72d7
ozanozbek-procore <ozan.ozbek@procore.com>
2025-02-11 15:04:11 +0100

    WIP

--- source/main.js
+++ source/main.js
@@ -4,10 +4,18 @@ import app from './app.js';
 import configuration from '../configuration.json' with { type: 'json' };
 
 const data = await app(configuration, true);
+let csv = '';
 
-const date = new Date();
-const filename = `data/data-${date.getFullYear()}${date.getMonth()}${date.getDate()}-${date.getHours()}${date.getMinutes()}.json`;
+for (const [repository, prs] of Object.entries(data)) {
+  prs.forEach(pr => {
+    const created = new Date(pr.created_at);
+    const closed = new Date(pr.closed_at);
+    csv += [repository, pr.url, Math.floor(created.getTime() / 1000), Math.floor(closed.getTime() / 1000)].join(',') + '\n'
+  });
+}
+
+const filename = `data/data-${Date.now()}.csv`;
 
 console.log(`Writing to file ${filename}...`);
-await fs.writeFile(`${filename}`, JSON.stringify(data, undefined, 2));
+await fs.writeFile(`${filename}`, csv);
 console.log('Done.');
